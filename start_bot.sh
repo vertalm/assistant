0a6b3393f9d36392cb7bfa841ea70f2710aa6f6d
Виталий Исаев <v.isaev@skyfoxy.com>
2023-11-17 01:16:16 +0700

    Assistant v0.1

--- start_bot.sh
+++ start_bot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -l
 export RACK_ENV=production
+source /home/vertalm/.rvm/environments/default
 cd /var/www/assistant
 /home/vertalm/.rvm/rubies/ruby-3.2.2/bin/bundle exec rake telegramgptbot:run_bot
