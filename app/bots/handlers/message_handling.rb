5459a9319307e95349b07a63c18bcb8fe6f4be35
Виталий Исаев <v.isaev@skyfoxy.com>
2023-11-17 09:48:42 +0700

    Assistant v0.1

--- app/bots/handlers/message_handling.rb
+++ app/bots/handlers/message_handling.rb
@@ -35,8 +35,8 @@ module MessageHandling
     puts "last_message: #{last_message}"
 
     # Если длина последнего сообщения больше 4096 символов, то разбиваем его на несколько сообщений
-    if last_message.length > 4096
-      last_message.scan(/.{1,4096}/m).each do |message_part|
+    if last_message.length > 4000
+      last_message.scan(/.{1,4000}/m).each do |message_part|
         bot.api.send_message(
           chat_id: message.chat.id,
           text: message_part
@@ -188,8 +188,8 @@ module MessageHandling
       last_message = OpenAiService.get_last_message(state.thread_id)
       puts "last_message: #{last_message}"
       # Если длина сообщения превышает 4096 символов, разбиваем его на несколько сообщений
-      if last_message.length > 4096
-        last_message.scan(/.{1,4096}/m) do |message_part|
+      if last_message.length > 4000
+        last_message.scan(/.{1,4000}/m) do |message_part|
           bot.api.send_message(
             chat_id: message.chat.id,
             text: message_part
