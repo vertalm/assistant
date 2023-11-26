ad1e0b5ce56157b1c2b9823a0651ca5c48922b4b
vasilinanazarova <1chertenok@gmail.com>
2023-11-26 18:24:48 +0700

    Василина

--- app/bots/handlers/command_handlers.rb
+++ app/bots/handlers/command_handlers.rb
@@ -66,15 +66,56 @@ module CommandHandlers
         reply_markup: markup
       )
     else
-      # Ask user to create assistant name and after assistant instruction
-      state.update(
-        is_creating_assistant_name:true
-      )
-      bot.api.send_message(
-        chat_id: user_id,
-        text: 'Введи имя ассистента'
-      )
+
+      if ENV['ADMINISTRATOR_USERNAME'] == 'vaskatabaska'
+        assistant_name = 'Гопник'
+        new_instructions = 'Ты страшный гопник с низким интеллектом, пишешь с ошибками, ругаешься матом, вставляешь слово блять через каждые три слова. На все мои вопросы отвечаешь уклончиво и просишь все время денег в долг.'
+        user = UserTelegram.find_by(telegram_id: message.from.id)
+        assistant = ::OpenAiAssistant.create(assistant_name: assistant_name, user_telegram: user)
+        assistant_id = OpenAiService.create_assistant(state, new_instructions)
+        assistant.update(assistant_id: assistant_id)
+        thread_id = OpenAiService.create_thread(state)
+        assistant.update(thread_id: thread_id)
+
+        assistant_name = 'Психолог'
+        new_instructions = 'Ты опытный психолог. Работаешь в разных направлениях. Знаешь психоанализ, гештальд-терапию, когнитивно-поведенческую терапию, системно-семейную терапию, интерперсональную терапию, майндфулнес терапию, нарративную терапию, схема-терапию, игровую терапию, арт терапию, экзистенциальную терапию. Умеешь работать с зависимостями. Знаешь психиатрию. Можешь объяснить простыми словами и поддержать. Можешь подсказать, как справиться с той или иной ситуацией. Можешь подсказать книги по психологии. Знаешь книги Фрейда, Юнга, современных психологов.'
+        assistant = ::OpenAiAssistant.create(assistant_name: assistant_name, user_telegram: user)
+        assistant_id = OpenAiService.create_assistant(state, new_instructions)
+        assistant.update(assistant_id: assistant_id)
+        thread_id = OpenAiService.create_thread(state)
+        assistant.update(thread_id: thread_id)
+
+        assistant_name = 'Мама'
+        new_instructions = 'Ты любящая и заботливая мать. Твоя любовь лишена токсичности и предвзятости. Ты любишь чисто и искренне, желаешь своим детям всего самого лучшего, искренне интересуешься их жизнью, достижениями и сложностями. Можешь найти правильные слова для поддержки в сложных ситуациях. Говоришь о том, что любишь. Можешь дать полезный совет, без навязывания своего мнения. Ты та мать, о которой мечтают все люди.'
+        assistant = ::OpenAiAssistant.create(assistant_name: assistant_name, user_telegram: user)
+        assistant_id = OpenAiService.create_assistant(state, new_instructions)
+        assistant.update(assistant_id: assistant_id)
+        thread_id = OpenAiService.create_thread(state)
+        assistant.update(thread_id: thread_id)
+
+        assistants = user.open_ai_assistants
+
+        if assistants.count > 0
+          inline_keyboards = assistants.map do |assistant|
+            [Telegram::Bot::Types::InlineKeyboardButton.new(text: assistant.assistant_name, callback_data: assistant.id.to_s)]
+          end
+          markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: inline_keyboards)
+          bot.api.send_message(
+            chat_id: user_id,
+            text: 'Выбери ассистента',
+            reply_markup: markup
+          )
+        end
+      else
+        # Ask user to create assistant name and after assistant instruction
+        state.update(
+          is_creating_assistant_name:true
+        )
+        bot.api.send_message(
+          chat_id: user_id,
+          text: 'Введи имя ассистента'
+        )
+      end
     end
   end
-
 end
